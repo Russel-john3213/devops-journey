f25b5fe99829cc3435ab75db52e1ac921ccc2c95
Russel-john3213 <russeljohnicomedez@gmail.com>
2026-04-18 03:49:51 +0800

    adding a autoclicker on player.py and adding mouse input recording to recorder.py using mouse library

--- python/tinytask-clone/player.py
+++ python/tinytask-clone/player.py
@@ -2,8 +2,18 @@ import pyautogui
 import time
 
 def start_playback():
+    while True:
+        try:
+            delay = float(input("Enter delay between clicks (seconds): "))
+            break
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
     print("Starting playback in 3 seconds...")
     time.sleep(3)
 
-    print("Clicking now!")
-    pyautogui.click()
+    print("Auto-clicking started. Press CTRL+C to stop.")
+
+    while True:
+        pyautogui.click()
+        time.sleep(delay)
--- python/tinytask-clone/recorder.py
+++ python/tinytask-clone/recorder.py
@@ -1,2 +1,10 @@
+import mouse
+
 def start_recording():
-    print("Recording started...")
+    print("Recording started... Press ESC to stop.")
+
+    mouse.hook(print)
+
+    mouse.wait('esc')
+
+    print("Recording stopped.")
