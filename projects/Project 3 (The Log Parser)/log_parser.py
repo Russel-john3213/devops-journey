e4a41d9ab0c1ee8ef35fc805a287b5d365d75ae7
Russel-john3213 <russeljohnicomedez@gmail.com>
2026-05-27 02:11:16 +0800

    adding a line where the summarize report instead of just printing in the terminal it will create a file and put the report in that text file

--- projects/Project 3 (The Log Parser)/log_parser.py
+++ projects/Project 3 (The Log Parser)/log_parser.py
@@ -8,3 +8,11 @@ with open("server.log", "r") as log_file:
             print(line.strip())
 
 print(f"Total errors found: {error_counter}")
+
+with open("incident_report.txt", "w") as report_file:
+    report_file.write(f"Total errors found: {error_counter}\n")
+    report_file.write("detailed log\n")
+    with open("server.log", "r") as log_file: 
+        for line in log_file:
+            if "ERROR" in line:
+                report_file.write(line.strip() + "\n")
